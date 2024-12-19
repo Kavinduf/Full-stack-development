7c52dd78d8b6866c9cc4bc025cecc928e9ad81f1
Kavindu Fernando <kavinduf2@gmail.com>
2024-12-19 21:53:47 +0530

    rest api

--- Backend/JSON/5.5 REST APIs/index.js
+++ Backend/JSON/5.5 REST APIs/index.js
@@ -1,4 +1,4 @@
-import express, { response } from "express";
+import express from "express";
 import axios from "axios";
 import bodyParser from "body-parser";
 
@@ -23,55 +23,88 @@ app.get("/", (req, res) => {
   res.render("index.ejs", { content: "Waiting for data..." });
 });
 
+
+// <--------------------------------------- GET Method-------------------------------------------------->
+
 app.post("/get-secret", async (req, res) => {
   const searchId = req.body.id;
   try {
     const result = await axios.get(API_URL + "/secrets/" + searchId, config);
     res.render("index.ejs", { content: JSON.stringify(result.data) });
   } catch (error) {
-    res.render("index.ejs", { content: JSON.stringify(error.response.data) });
+    console.log("Failed to make request:", error.message);
   }
 });
+
+
+// <--------------------------------------- POST Method-------------------------------------------------->
 
 app.post("/post-secret", async (req, res) => {
   // TODO 2: Use axios to POST the data from req.body to the secrets api servers.
 
-  let request = req.body;
-
-  // let data = {
-  //   id: request.id,
-  //   secret: request.secret,
-  //   emScore: request.score
-  // }
-
   try{
-    const response = await axios.post(API_URL + "/secrets", req.body, config );
+    const result = await axios.post(API_URL + "/secrets", req.body, config );
 
-    let result = response.data;
-    console.log(result)
-
-    if(result){
-      res.render("index.ejs", {content: JSON.stringify(result)})
-    }
+      res.render("index.ejs", {content: JSON.stringify(result.data)})
 
   }catch (error) {
     console.log("Failed to make request:", error.message);
   }
 });
 
+
+// <--------------------------------------- PUT Method-------------------------------------------------->
+
 app.post("/put-secret", async (req, res) => {
   const searchId = req.body.id;
-  // TODO 3: Use axios to PUT the data from req.body to the secrets api servers.
+  try {
+    const result = await axios.put(
+      API_URL + "/secrets/" + searchId,
+      req.body,
+      config
+    );
+    res.render("index.ejs", { content: JSON.stringify(result.data) });
+  } catch (error) {
+    console.log("Failed to make request:", error.message);
+  }
 });
+
+
+// <--------------------------------------- PATCH Method-------------------------------------------------->
 
 app.post("/patch-secret", async (req, res) => {
   const searchId = req.body.id;
   // TODO 4: Use axios to PATCH the data from req.body to the secrets api servers.
+
+  try {
+    const result = await axios.patch(
+      API_URL + "/secrets/" + searchId,
+      req.body,
+      config
+    );
+    res.render("index.ejs", { content: JSON.stringify(result.data) });
+  } catch (error) {
+    console.log();
+  }
 });
+
+
+// <--------------------------------------- DELETE Method-------------------------------------------------->
 
 app.post("/delete-secret", async (req, res) => {
   const searchId = req.body.id;
   // TODO 5: Use axios to DELETE the item with searchId from the secrets api servers.
+
+  try{
+    const result = await axios.delete(
+      API_URL + "/secrets/" + searchId,
+      config
+    )
+
+    res.render("index.ejs", {content: JSON.stringify(result.data)})
+  }catch (error) {
+    console.log("Failed to make request:", error.message);
+  }
 });
 
 app.listen(port, () => {
